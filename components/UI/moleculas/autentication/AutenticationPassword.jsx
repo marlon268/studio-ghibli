012dc3e297df0e0452966118486a3ccc65ec0a2b
marlon268 <varon.marlon83@gmail.com>
2022-04-21 12:21:13 -0500

    intento del primer despliegue

--- components/UI/moleculas/autentication/AutenticationPassword.jsx
+++ components/UI/moleculas/autentication/AutenticationPassword.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { InputBase } from '../atomos/InputBase';
+import React from "react";
+import { InputBase } from "../atomos/InputBase";
 
 export const AutenticationPassword = () => {
-	return (
-		<div className="autenticationPassword">
-			<label text="Password" />
-			<InputBase img="icon password" type="password" /> // falta icono
-		</div>
-	);
+  return (
+    <div className="autenticationPassword">
+      <label text="Password" />
+      <InputBase img="icon password" type="password" />
+    </div>
+  );
 };
--- components/UI/organismos/Modal/ModalContainer.jsx
+++ components/UI/organismos/Modal/ModalContainer.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import {ModalTextContainer} from "../../moleculas/ModalQualify/ModalTextContainer"
-import {ModalIconsContainer} from "../../moleculas/ModalQualify/ModalIconsContainer"
-import "./ModalContainer.scss"
+import React from "react";
+import { ModalTextContainer } from "../../moleculas/ModalQualify/ModalTextContainer";
+import { ModalIconsContainer } from "../../moleculas/ModalQualify/ModalIconsContainer";
+import "./ModalContainer.scss";
 
 export const ModalContainer = () => {
   return (
-    <div class="modal">
-        <ModalTextContainer />
-        <ModalIconsContainer />
+    <div className="modal">
+      <ModalTextContainer />
+      <ModalIconsContainer />
     </div>
-  )
-}
+  );
+};
