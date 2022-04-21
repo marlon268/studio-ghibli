57f4112df12b16b4f8c1755c871d13a765588aa9
marlon268 <varon.marlon83@gmail.com>
2022-04-21 16:38:06 -0500

    key agregada en home

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,45 +1,41 @@
-import React, { useState, useEffect }from 'react'
-import {HeaderProfile} from '../components/UI/organismos/HeaderProfile/HeaderProfile'
-import {getList} from '../database/services'
-import HomeCards from '../components/UI/organismos/HomeCards/HomeCards'
+import React, { useState, useEffect } from "react";
+import { HeaderProfile } from "../components/UI/organismos/HeaderProfile/HeaderProfile";
+import { getList } from "../database/services";
+import HomeCards from "../components/UI/organismos/HomeCards/HomeCards";
 
-
-
- 
 const Home = () => {
-  
   const [list, setList] = useState([]);
 
   useEffect(() => {
-    let mounted = true
-    getList()
-      .then(films => {
-        console.log('films:', films)
-        if (mounted) {
-       setList(films)
-       }
-      })
+    let mounted = true;
+    getList().then((films) => {
+      console.log("films:", films);
+      if (mounted) {
+        setList(films);
+      }
+    });
     return () => {
-      mounted = false
-    }
-  }, [])
-  
-  return (
-    
-     <div className="animation">
-      <div className="containerCards">
-      <HeaderProfile /> 
-      {
-            list.map((film) => (
-              <HomeCards id={film.id} title={film.title} image={film.image}  release_date={film.release_date} description={film.description} />
-            ))
-      } 
-      </div>
-        
-        </div>   
-  
-     
-  )
-}
+      mounted = false;
+    };
+  }, []);
 
-export default Home
+  return (
+    <div className="animation">
+      <div className="containerCards">
+        <HeaderProfile />
+        {list.map((film) => (
+          <HomeCards
+            key={film.id}
+            id={film.id}
+            title={film.title}
+            image={film.image}
+            release_date={film.release_date}
+            description={film.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
