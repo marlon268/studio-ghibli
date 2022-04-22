a34572abce73bb27d2da25289b8d43c66edb729d
cherced <Chernandezcediel@gmail.com>
2022-04-22 14:23:51 -0500

    BugFixing

--- components/UI/atomos/HomeCards/YearCard.jsx
+++ components/UI/atomos/HomeCards/YearCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MovieWatched } from "../MovieWatched/movieWatched";
+import { MovieWatched } from "../MovieWatched/MovieWatched";
 
 export const YearCard = ( props ) =>{
     return(
