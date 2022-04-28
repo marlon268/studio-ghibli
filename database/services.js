24d6916234dd45ae55161e864cd7f30cac348c32
Jimmy Osma <josma109@gmail.com>
2022-04-28 17:53:27 -0500

    update database with main

--- database/services.js
+++ database/services.js
@@ -1,6 +1,12 @@
-export function getList (){
-    return (
-        fetch(' https://demo5947973.mockable.io/movies')
-            .then(data => data.json())
-    ) 
+export async function getList (){
+    try {
+        return (
+            fetch('https://demo5947973.mockable.io/movies')
+                .then(data => data.json())
+        )         
+    } catch (error) {
+        console.log(error);
+        console.log("Error en la conexión");
+    }
+    
 }
