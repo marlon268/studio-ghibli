a38aadcb355dbd96a7054c42888b245279c06ac7
cherced <Chernandezcediel@gmail.com>
2022-04-29 16:28:36 -0500

    Crossing the finggers

--- components/UI/organismos/HeaderDetails/HeaderDetails.jsx
+++ components/UI/organismos/HeaderDetails/HeaderDetails.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import  Link  from 'next/link'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
 
 export const HeaderDetails = () => {
   return (
-    <header>
+    <header className="HeaderDetails">
   <button className="UserNameOptions__button">
     <Link href="/home">
       <FontAwesomeIcon icon={faArrowLeft} width="25px" color='white'/>
