dbd469743ea49d3882f448295783ec1104abf5e5
cherced <Chernandezcediel@gmail.com>
2022-05-15 20:48:43 -0500

    Logic to the Filter

--- components/UI/organismos/FilterContainer/FilterContainer.jsx
+++ components/UI/organismos/FilterContainer/FilterContainer.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { FilterSearch } from '../../moleculas/FilterSearch/FilterSearch'
 
-export const FilterContainer = () => {
+export const FilterContainer = (props) => {
   return (
     <div className="FilterSearch_Container" > 
-        <FilterSearch />
+        <FilterSearch getOption={props.getOption} getOrder={props.getOrder} handleChange={props.handleChange}/>
     </div>
   )
 }
