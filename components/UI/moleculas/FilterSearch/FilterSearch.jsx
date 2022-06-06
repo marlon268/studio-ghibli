4391104be1ea83fcff362e068aef5983ac0df4da
cherced <Chernandezcediel@gmail.com>
2022-06-05 20:49:52 -0500

    WE ARE CLOSSE TO THE FIRST REAL REAL FINISH OF VERY GOOD CV

--- components/UI/moleculas/FilterSearch/FilterSearch.jsx
+++ components/UI/moleculas/FilterSearch/FilterSearch.jsx
@@ -5,18 +5,22 @@ export const FilterSearch = (props) => {
     <React.Fragment>
     <div className="select" >
     <label>Sort By </label>
+    <div className="BoxFilter">
     <select id="sortBy" name="SortBy" onClick={props.handleChange}  onChange={props.getOption}>
       <option disabled="choice" selected="selected">Select One Option</option>
               <option value="Year">Year</option>
               <option value="Gender">Gender</option>
               <option value="Character">Character</option>              
      </select>   
+     </div>
      <label>Order  </label>
+     <div className="BoxFilter">
     <select id="order" name="Order" onClick={props.handleChange} onChange={props.getOrder}>
       <option disabled="choice" selected="selected">Select One Option</option>
                 <option value="Ascending" >Ascending</option>
                 <option value="Falling" >Falling</option>
      </select>   
+     </div>
     </div>    
     </React.Fragment>
   )
